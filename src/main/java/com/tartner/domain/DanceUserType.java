0f4c2436cda1877cd85ff31f7674f7b4602b13a0
Bill Davis <bdavisx@yahoo.com>
2015-04-11 16:27:07 -0500

    Initial commit

---
+++ src/main/java/com/tartner/domain/DanceUserType.java
@@ -0,0 +1,10 @@
+package com.tartner.domain;
+
+public enum DanceUserType {
+    Administrator,
+    ParentGuardian,
+    CompanyMember,
+    JuniorCompanyMember,
+    ApprenticeCompanyMember,
+    PerformanceCompanyMember
+}
