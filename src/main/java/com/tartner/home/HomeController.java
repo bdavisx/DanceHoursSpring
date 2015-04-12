6c6b651e755bc238920e315ef379a0060f5dc501
Bill Davis <bdavisx@yahoo.com>
2015-04-12 11:56:44 -0500

    Checking in before completely switching off of spring io platform - not actually working right now but...

--- src/main/java/com/tartner/home/HomeController.java
+++ src/main/java/com/tartner/home/HomeController.java
@@ -2,14 +2,29 @@ package com.tartner.home;
 
 import java.security.Principal;
 
+import com.tartner.domain.DanceUser;
+import com.tartner.domain.DanceUserType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 @Controller
 public class HomeController {
-	
+	@PersistenceContext
+	private EntityManager em;
+
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Principal principal) {
+		final DanceUser danceUser = new DanceUser();
+		danceUser.setFirstName( "Bill" );
+		danceUser.setLastName( "Davis" );
+		danceUser.setEmail( "bdavisx@yahoo.com" );
+		danceUser.setIsActive( true );
+		danceUser.setUserType( DanceUserType.Administrator );
+		em.persist( danceUser );
+
 		return principal != null ? "home/homeSignedIn" : "home/homeNotSignedIn";
 	}
 }
