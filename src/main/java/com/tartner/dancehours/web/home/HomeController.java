5b4c63b885023a03ca8691c9c393f29c93358e5f
Bill Davis <bdavisx@yahoo.com>
2015-04-15 22:13:37 -0500

    Jooq working; generation using pom dependencies and a test insert from a controller. Now to build the rest of the tables and data access stuff.

--- src/main/java/com/tartner/dancehours/web/home/HomeController.java
+++ src/main/java/com/tartner/dancehours/web/home/HomeController.java
@@ -1,5 +1,7 @@
 package com.tartner.dancehours.web.home;
 
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,6 +10,7 @@ import java.security.Principal;
 
 @Controller
 public class HomeController {
+	@Autowired DSLContext dsl;
 
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String index(Principal principal) {
--- src/main/java/com/tartner/dancehours/web/login/LoginController.java
+++ src/main/java/com/tartner/dancehours/web/login/LoginController.java
@@ -1,14 +1,32 @@
 package com.tartner.dancehours.web.login;
 
+import org.jooq.DSLContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.UUID;
+
+import static com.tartner.dancehours.database.Tables.DANCE_USER;
+
 @Controller
 public class LoginController {
+    @Autowired
+    DSLContext dsl;
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String get() {
+        dsl.insertInto( DANCE_USER )
+            .set( DANCE_USER.USER_ID, UUID.randomUUID() )
+            .set( DANCE_USER.EMAIL, "b@c.com" )
+            .set( DANCE_USER.FIRST_NAME, "Bill")
+            .set( DANCE_USER.LAST_NAME, "Davis" )
+            .set( DANCE_USER.IS_ACTIVE, true )
+            .set( DANCE_USER.USER_TYPE, "" )
+            .set( DANCE_USER.PASSWORD_HASH, 0L )
+            .execute();
+
         return "login/login";
     }
 
