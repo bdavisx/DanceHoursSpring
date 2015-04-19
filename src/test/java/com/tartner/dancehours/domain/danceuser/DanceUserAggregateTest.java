7c01797faf5f80c9ba452e4e0124d2b6b4847f91
Bill Davis <bdavisx@yahoo.com>
2015-04-19 15:27:36 -0500

    Duplication checks tested; need to refactor now.

--- src/test/java/com/tartner/dancehours/domain/danceuser/DanceUserAggregateTest.java
+++ src/test/java/com/tartner/dancehours/domain/danceuser/DanceUserAggregateTest.java
@@ -3,6 +3,8 @@ package com.tartner.dancehours.domain.danceuser;
 import com.tartner.dancehours.domain.danceuser.external.CreateDanceUserCommand;
 import com.tartner.dancehours.domain.danceuser.external.DanceUserAggregateQueryModel;
 import com.tartner.dancehours.domain.danceuser.external.DanceUserCreatedEvent;
+import com.tartner.dancehours.domain.danceuser.external.DanceUserEmailAlreadyExistsException;
+import com.tartner.dancehours.domain.danceuser.external.DanceUserIdAlreadyExistsException;
 import org.axonframework.domain.DomainEventMessage;
 import org.axonframework.domain.DomainEventStream;
 import org.axonframework.test.FixtureConfiguration;
@@ -14,25 +16,22 @@ import java.util.UUID;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 public class DanceUserAggregateTest {
 
-    public static final String CreateUserId =
-        "e78320a0-48b9-490a-8b1b-f5bcdcf34995";
+    private static String UUIDString = "e78320a0-48b9-490a-8b1b-f5bcdcf34995";
+    public static final UUID CreateUserId = UUID.fromString( UUIDString );
     public static final String CreateEmail = "bdavisx@yahoo.com";
     public static final String CreateFirstName = "Bill";
     public static final String CreateLastName = "Davis";
     public static final String CreatePassword = "notReal";
 
     private FixtureConfiguration fixture;
-    private UUID userId;
 
     @Before
     public void setUp() throws Exception {
         fixture = Fixtures.newGivenWhenThenFixture( DanceUserAggregate.class );
-        userId = UUID.fromString( CreateUserId );
     }
 
     @Test
@@ -50,11 +49,11 @@ public class DanceUserAggregateTest {
             mock( DanceUserAggregateQueryModel.class );
 
         DanceUserAggregate user = new DanceUserAggregate();
-        user.initialize( userId, CreateEmail, CreateLastName, CreateFirstName,
+        user.initialize( CreateUserId, CreateEmail, CreateLastName, CreateFirstName,
             queryModelMock );
 
         verify( queryModelMock ).emailAlreadyExists( CreateEmail );
-        verify( queryModelMock ).userIdAlreadyExists( userId );
+        verify( queryModelMock ).userIdAlreadyExists( CreateUserId );
 
         final DomainEventStream events = user.getUncommittedEvents();
         final DomainEventMessage next = events.next();
@@ -62,8 +61,40 @@ public class DanceUserAggregateTest {
         assertThat( event, equalTo( payload ) );
     }
 
+    @Test( expected = DanceUserIdAlreadyExistsException.class )
+    public void danceUserCreatedDuplicateUserId() throws Exception {
+        CreateDanceUserCommand command = createValidCreateCommand();
+
+        DanceUserCreatedEvent event =
+            createCreatedEventForValidCommand( command );
+
+        DanceUserAggregateQueryModel queryModelMock  =
+            mock( DanceUserAggregateQueryModel.class );
+        when( queryModelMock.userIdAlreadyExists( CreateUserId ) ).thenReturn( true );
+
+        DanceUserAggregate user = new DanceUserAggregate();
+        user.initialize( CreateUserId, CreateEmail, CreateLastName, CreateFirstName,
+            queryModelMock );
+    }
+
+    @Test( expected = DanceUserEmailAlreadyExistsException.class )
+    public void danceUserCreatedDuplicateEmail() throws Exception {
+        CreateDanceUserCommand command = createValidCreateCommand();
+
+        DanceUserCreatedEvent event =
+            createCreatedEventForValidCommand( command );
+
+        DanceUserAggregateQueryModel queryModelMock  =
+            mock( DanceUserAggregateQueryModel.class );
+        when( queryModelMock.emailAlreadyExists( CreateEmail ) ).thenReturn( true );
+
+        DanceUserAggregate user = new DanceUserAggregate();
+        user.initialize( CreateUserId, CreateEmail, CreateLastName, CreateFirstName,
+            queryModelMock );
+    }
+
     private CreateDanceUserCommand createValidCreateCommand() {
-        CreateDanceUserCommand command = new CreateDanceUserCommand( userId );
+        CreateDanceUserCommand command = new CreateDanceUserCommand( CreateUserId );
         command
             .email( CreateEmail )
             .firstName( CreateFirstName )
@@ -75,7 +106,7 @@ public class DanceUserAggregateTest {
     private DanceUserCreatedEvent createCreatedEventForValidCommand(
         final CreateDanceUserCommand command ) {
         DanceUserCreatedEvent event = new DanceUserCreatedEvent();
-        event.setUserId( userId );
+        event.setUserId( CreateUserId );
         event.setEmail( command.getEmail() );
         event.setFirstName( command.getFirstName() );
         event.setLastName( command.getLastName() );
