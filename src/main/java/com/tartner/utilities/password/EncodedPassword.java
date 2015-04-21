30bba6c188d3025c0a6ae051b9db1f49eca75f7c
Bill Davis <bdavisx@yahoo.com>
2015-04-20 23:22:30 -0500

    Just a Note for next step.

--- src/main/java/com/tartner/utilities/password/EncodedPassword.java
+++ src/main/java/com/tartner/utilities/password/EncodedPassword.java
@@ -3,6 +3,9 @@ package com.tartner.utilities.password;
 import java.util.Arrays;
 
 public class EncodedPassword {
+    // TODO: need to create passwordHash and Salt classes, make sure
+    // those and this one are compatible w/ XStream; then change the
+    // event to have an EncodedPassword instead of the separate properties
     private final byte[] passwordHash;
     private final byte[] salt;
 
