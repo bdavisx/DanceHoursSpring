a1b3641aeca5783ec94a6a19426015225fcee4f0
Bill Davis <bdavisx@yahoo.com>
2015-04-25 16:31:11 -0500

    Integration test for PasswordProjector working; but it's not deleting the record after the test.

--- src/test/java/com/tartner/domain/password/PasswordProjectorTest.java
+++ src/test/java/com/tartner/domain/password/PasswordProjectorTest.java
@@ -1,18 +1,31 @@
 package com.tartner.domain.password;
 
+import com.tartner.IntegrationTestCategory;
+import com.tartner.dancehours.querymodel.database.tables.daos.AggregatePasswordsDao;
+import com.tartner.dancehours.querymodel.database.tables.pojos.AggregatePasswords;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes=StandardIntegrationTestConfiguration.class)
 @Transactional
+@Category( IntegrationTestCategory.class )
 public class PasswordProjectorTest {
+    @Autowired private AggregatePasswordsDao dao;
+    @Autowired PasswordProjector projector;
 
     @Before
     public void setUp() throws NoSuchAlgorithmException {
@@ -20,7 +33,22 @@ public class PasswordProjectorTest {
 
     @Test
     public void temp() throws Exception {
+        TestPasswordHolder holder = TestPasswordHolder.CreateDefaultTest();
 
+        final UUID uuid = UUID.randomUUID();
+
+        final PasswordSetEvent event = new PasswordSetEvent( uuid,
+            holder.getPasswordHash(), holder.getSalt() );
+        projector.handle( event );
+
+        final Optional<AggregatePasswords> first =
+            dao.fetchByAggregateId( uuid ).stream().findFirst();
+
+        assertThat( first.isPresent(), is( true ) );
+
+        final AggregatePasswords password = first.get();
+        assertThat( password.getPasswordHash(), is( holder.passwordHash ) );
+        assertThat( password.getSalt(), is( holder.salt ) );
     }
 
 }
