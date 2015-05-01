0f8558e0b06d345de606eff23372a819e6206f16
Bill Davis <bdavisx@yahoo.com>
2015-05-01 11:01:43 -0500

    Form validation working.

--- src/main/java/com/tartner/dancehours/web/login/CreateAccountController.java
+++ src/main/java/com/tartner/dancehours/web/login/CreateAccountController.java
@@ -1,13 +1,19 @@
 package com.tartner.dancehours.web.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+
 @Controller
 public class CreateAccountController {
+    @Autowired private CreateAccountFormValidator validator;
+
     @RequestMapping(value = "/createAccount", method = RequestMethod.GET)
     public String get(Model model) {
         CreateAccountForm initialForm = new CreateAccountForm();
@@ -15,11 +21,22 @@ public class CreateAccountController {
         return "login/createAccount";
     }
 
+    // TODO: add validation here
+    // TODO: add command gateway, create / send command
+    // TODO: login user
+    // TODO: redirect to home
+
     @RequestMapping(value = "/createAccount", method = RequestMethod.POST)
     public String post(
-        @ModelAttribute("createAccountForm") CreateAccountForm form ) {
+        @ModelAttribute("createAccountForm") @Valid CreateAccountForm form,
+        BindingResult bindingResult ) {
 
-        return "login/createAccount";
+        validator.validate( form, bindingResult );
+
+        if (bindingResult.hasErrors()) {
+            return "login/createAccount";
+        }
+        return "/home/homeSignedIn";
     }
 
 }
