47ea3fe187f31d54d994010be57ca83d02ae5a77
Bill Davis <bdavisx@yahoo.com>
2015-05-01 15:09:05 -0500

    Setup fake login to move things along.

--- src/main/java/com/tartner/dancehours/web/login/LoginController.java
+++ src/main/java/com/tartner/dancehours/web/login/LoginController.java
@@ -1,8 +1,16 @@
 package com.tartner.dancehours.web.login;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Arrays;
 
 @Controller
 public class LoginController {
@@ -13,8 +21,19 @@ public class LoginController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String post() {
+    public String post( @RequestParam("username") String username,
+        @RequestParam("password") String password ) {
 
-        return "login/createAccount";
+        final User user = new User( username, password, Arrays.asList(
+            new GrantedAuthority[] {
+                new GrantedAuthority() {
+                    public String getAuthority() { return "ROLE_USER"; }
+                } } ) );
+        Authentication authentication =
+            new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return "redirect:/";
     }
 }
